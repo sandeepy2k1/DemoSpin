5ad5048234c38b9c1efb40d2032074737b61b617
Sandeep Saurabh <sandeepsaurabh@SandeepSaurabh.local>
2014-04-15 14:21:58 +0530

    Initial Commit

---
+++ DemoSpin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoSpin
+//
+//  Created by Sandeep Saurabh on 15/04/14.
+//  Copyright (c) 2014 Sandeep Saurabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoSpin/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoSpin
+//
+//  Created by Sandeep Saurabh on 15/04/14.
+//  Copyright (c) 2014 Sandeep Saurabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
